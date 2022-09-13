5c6e2ae85494dc385aef2eb0182d8c3a776e9ebe
GuidoCostaDF <guidocostadf@gmail.com>
2022-09-13 09:32:56 -0300

    preenchimento automatico dos campos

--- script.js
+++ script.js
@@ -5,9 +5,23 @@ async function buscaEndereco(cep) {
     if (consultaCepConvertida.erro) {
       throw Error("CEP não existente!");
     }
+
+    var cidade = document.getElementById("cidade");
+    var logradouro = document.getElementById("endereco");
+    var estado = document.getElementById("estado");
+    var bairro = document.getElementById("bairro");
+
+    cidade.value = consultaCepConvertida.localidade;
+    logradouro.value = consultaCepConvertida.logradouro;
+    estado.value = consultaCepConvertida.uf;
+    bairro.value = consultaCepConvertida.bairro;
+
     console.log(consultaCepConvertida);
     return consultaCepConvertida;
   } catch (erro) {
     console.log(erro);
   }
 }
+
+var cep = document.querySelector("#cep");
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
